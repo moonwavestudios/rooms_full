7cd87901e946e221ff9875c48fa15f01f60f52af
Virus01Official <virusidkoff@gmail.com>
2026-05-20 18:31:32 +0200

    finished the collab

--- door.gd
+++ door.gd
@@ -16,7 +16,7 @@ func try_open(interactor: Node) -> void:
 	if locked:
 		if not interactor.player_has_key():
 			$LockedSound.play()
-			$ProximityPrompt._activation_count = 0
+			$ProximityPrompt.reset_activations()
 			return
 		interactor.consume_key()
 		locked = false
