134cc90e814a2b2f9c198b298e7c610d6c2e4b80
Virus01Official <virusidkoff@gmail.com>
2026-07-23 19:57:49 +0200

    working on the lobby

--- lobby.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-func _ready():
-	$UI/StartButton.visible = multiplayer.is_server()
-	pass
-
-func _on_start_button_pressed():
-	if multiplayer.is_server():
-		get_parent().rpc("rpc_load_game")
